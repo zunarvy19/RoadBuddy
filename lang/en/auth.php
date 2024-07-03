ddb5627ee4d49db9f13fd906abc7fa0608014353
zunarvy19 <arvy977#gmail.com>
2024-07-03 22:50:10 +0700

    update message

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Username atau password tidak valid.',
+    'password' => 'Password tidak valid.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
