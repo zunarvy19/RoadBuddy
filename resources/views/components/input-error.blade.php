21d190b2a4346dd860a5c6b18f44ffc7292fff06
zunarvy19 <arvy977#gmail.com>
2024-07-03 22:57:04 +0700

    feat inputError

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,5 +1,6 @@
 @props(['messages'])
 
+@if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
